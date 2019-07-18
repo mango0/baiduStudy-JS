67d5ab255b38dd0a5be7df3b89dd9d7c7da7474c
mango0 <15802127035@163.com>
2019-07-18 23:06:48 +0800

    全选的修改

--- Lesson31-33/js/index.js
+++ Lesson31-33/js/index.js
@@ -96,7 +96,7 @@ let checkboxWrapper = document.getElementsByClassName('checkbox-wrapper');
 let checkboxInput = checkboxWrapper[0].getElementsByTagName('input');
 
 // 点击全选时，如果单个选项中只要有一个不是被选上的状态，则进行全选操作
-allSelect.addEventListener('change', function() {
+/*allSelect.addEventListener('change', function() {
     // console.log(allSelect.checked);
 
     if(allSelect.checked) {
@@ -111,7 +111,7 @@ allSelect.addEventListener('change', function() {
             }
         }
     }
-})
+})*/
 
 // 事件委托，监听多选框
 checkboxWrapper[0].addEventListener('change', function(e) {
@@ -120,17 +120,43 @@ checkboxWrapper[0].addEventListener('change', function(e) {
 
     let input = target.tagName.toLowerCase();
     if(input === 'input') {
-        console.log(target.checked,'checked');
 
-        for(let i = 0;i < checkboxInput.length;i++) {
-
-            // 点击全选时，如果单个选项中所有选项都已经是被选上的状态，则无反应
-            if(checkboxInput[i].checked) {
-                allSelect.checked = true;
+        // 有这个自定义属性说明是全选框
+        if(target.dataset.type === 'all') {
+            // 如果选中全选框
+            if(target.checked) {
+                // 选中所有的多选框
+                for(let i = 0;i < checkboxInput.length;i++) {
+                    checkboxInput[i].checked = true;
+                }
+            }else {
+                for(let i = 0;i < checkboxInput.length;i++) {
+                    // 1。点击全选时，如果单个选项中所有选项都已经是被选上的状态，则无反应
+                    if(checkboxInput[i].checked) {
+                        target.checked = true;
+                    }
+                }
             }
-            // 如果当前是全选状态，取消任何一个子选项，则全选CheckBox也要置为未勾选状态
-            else {
+        }else {
+            // 2。如果当前是全选状态，取消任何一个子选项，则全选CheckBox也要置为未勾选状态
+            if(!target.checked) {
                 allSelect.checked = false;
+            }
+
+            let count = 0;
+            for(let i = 0;i < checkboxInput.length;i++) {
+                if(checkboxInput[i].checked) {
+                    count++;
+                }
+
+                // 3。点击最后一个未被选中的单个选项后，全选CheckBox也要置为被勾选状态
+                if(count === checkboxInput.length) {
+                    allSelect.checked = true;
+                }
+            }
+            // 4。不允许一个都不勾选，所以当用户想取消唯一一个被勾选的子选项时，无交互反应，不允许取消勾选
+            if(count === 0) {
+                target.checked = true;
             }
         }
     }
